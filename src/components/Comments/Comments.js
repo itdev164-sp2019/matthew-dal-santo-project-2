3fbff79a00fa79d7955bbf7545a63162fd957186
Matthew Dal Santo <SudoSaint@gmail.com>
2019-04-16 01:35:15 -0500

    Sketched out Comment look.

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,7 +5,8 @@ import {
     Image,
     Heading,
     Text,
-    Flex
+    Flex,
+    Button
   } from 'rebass'
 import styled from 'styled-components'
 //import { Edit } from 'styled-icons/feather'
@@ -23,12 +24,38 @@ export class Comments extends Component {
     render() {
         return (
             <Box>
-                <CommentBody my={4}>
-                    <Text>Comment Body Test</Text>
+                <CommentBody my={4} p={1}>
+                    <Box>
+                        <Text>Comments (Number)</Text>
+                    </Box>
+                    <Flex>
+                        <Image height="30px" width="30px" src="https://cdn-images-1.medium.com/max/600/1*TIbS8VWW5X4THclziAedPw.jpeg"/>
+                        <input defaultValue="Name (required)" type="" name="name" />
+                        <input defaultValue="Location (required)" type="" name="location" />
+                    </Flex>
+                    <Box>
+                        <input type="text" name="name" />
+                    </Box>
+                    <Flex>
+                        <Button>CANCEL</Button>
+                        <Button>SUBMIT</Button>
+                    </Flex>
+                </CommentBody>
+                <CommentBody mb={4}>
+                    <Flex>
+                        <Box>
+                            <Image height="30px" width="30px" src="https://cdn-images-1.medium.com/max/600/1*TIbS8VWW5X4THclziAedPw.jpeg"/>
+                        </Box>
+                        <Box>
+                            <Text>Name</Text>
+                            <Text>Location</Text>
+                        </Box>
+                    </Flex>
+                    <Box pl={4}>
+                        <Text>Comment Text</Text>
+                    </Box>
                 </CommentBody>
             </Box>
         );      
     }
 }
-
-export default Comments;
