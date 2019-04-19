6ad47ba3f754f10b87a4b65d649e6b2a0dfb38b0
Matthew Dal Santo <SudoSaint@gmail.com>
2019-04-19 00:29:54 -0500

    getting really frustrated with Gatsby.  I have no idea why I can't get state to work with my component.

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -11,7 +11,6 @@ import {
 import styled from 'styled-components'
 //import { Edit } from 'styled-icons/feather'
 
-import CommentSubmit from './CommentSubmit'
 import CommentMessage from './CommentMessage'
 
 const CommentTitle = styled(Text)`
@@ -23,12 +22,112 @@ const CommentTitle = styled(Text)`
     background-color: grey;
  `
 
+const defaultName = 'Name (required)';
+const defaultLocation = 'Location (required)';
+const defaultMessage = ' ';
+
 export class Comments extends Component {
+
+    state = {
+        commentSubmit: [
+            {
+                name: defaultName,
+                location: defaultLocation,
+                message: defaultMessage
+            }
+        ],
+        comments: []
+    }
+
+    // constructor(props) {
+    //     super(props);
+    //     //this.resetSubmit = this.resetSubmit.bind(this);
+    //     //this.changeSelectedRecipe = this.changeSelectedRecipe.bind(this);
+    //     //this.toggleModal = this.toggleModal.bind(this);
+
+    //     this.defaultName = 'Name (required)';
+    //     this.defaultLocation = 'Location (required)';
+    //     this.defaultMessage = ' ';
+
+    //     this.state = {
+    //         commentSubmit: [
+    //             {
+    //                 name: this.defaultName,
+    //                 location: this.defaultLocation,
+    //                 message: this.defaultMessage
+    //             }
+    //         ],
+    //         comments: []
+    //     }
+    //   }
+    
+    //   changeSelectedRecipe(index) {
+    //     this.setState({
+    //       selectedRecipe: index
+    //     })
+    //   }
+    
+    //   toggleModal() {
+    //     this.setState({
+    //       isModal: !this.state.isModal
+    //     })
+    //   }
+    
+    resetSubmit() {
+        this.setState( {
+            name: defaultName,
+            location: defaultLocation,
+            message: defaultMessage
+        })
+        document.getElementById('name').value = defaultName; 
+        document.getElementById('location').value = defaultLocation; 
+        document.getElementById('message').value = defaultMessage; 
+    }
+
+    commentNumberDisplay() {
+        if (this.state.comments.length < 1) {
+            return 'There are no comments on this article yet.  Be the first to comment!';
+        } else {
+            return 'Comments' + this.state.comments.length
+        }
+    }
+
     render() {
+        
         return (
             <Box>
-                
-                <CommentSubmit />
+                <CommentBody my={4} p={2}>
+                    <Box>
+                        <Text>{this.commentNumberDisplay()}</Text>
+                    </Box>
+                    <Flex>
+                        <Image height="30px" width="30px" src="https://cdn-images-1.medium.com/max/600/1*TIbS8VWW5X4THclziAedPw.jpeg" />
+
+                        <input defaultValue={defaultName} type="text" name="name" id="name"
+                        value={this.state.commentSubmit.name}
+                        onChange={e => this.setState({ name: e.target.value })} />
+
+                        <input defaultValue={defaultLocation} type="text" name="location" id="location"
+                        value={this.state.commentSubmit.location}
+                        onChange={e => this.setState({ location: e.target.value })} />
+                    </Flex>
+                    <Box>
+                        <input defaultValue={defaultMessage} type="text" name="message" id="message" autoComplete="off"
+                        value={this.state.commentSubmit.message}
+                        onChange={e => this.setState({ message: e.target.value })} />
+                    </Box>
+                    <Flex>
+                        <Button onClick={() => this.resetSubmit()}>CANCEL</Button>
+                        <Button>SUBMIT</Button>
+                    </Flex>
+                    <Box>
+                        <Text>Test</Text>
+                        <Text>Default Name: {this.defaultName}</Text>
+                        <Text>State Name: {this.state.commentSubmit.name}</Text>
+                        <Text>State Location: {this.state.commentSubmit.location}</Text>
+                        <Text>State Message: {this.state.commentSubmit.message}</Text>
+                    </Box>
+                </CommentBody>
 
                 <CommentMessage />
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import SEO from "../components/seo"
 import { Comments } from '../components/Comments/Comments'
 
 const IndexPage = () => (
+  
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
